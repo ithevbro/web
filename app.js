03beacc9a941bc9c4859f3b0ef464c9f3b6a65de
ithevbro <ithevbro@gmail.com>
2024-03-21 12:42:12 +0200

    start of backend

--- app.js
+++ app.js
@@ -2,6 +2,32 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 const path = require('path')
+const { MongoClient } = require('mongodb')
+const uri = 'mongodb://localhost:27017'
+const client = new MongoClient(uri)
+const dbName = 'test'
+
+
+async function run() {
+    try {
+        const database = await client.db(dbName);
+        const hohol = await database.collection('hohol')
+        await hohol.insertOne(
+            {
+                imgpath: 'burger.svg',
+                price: 600,
+                title: 'Мясная бомба',
+                weight: '520г',
+                description: 'blablablaba',
+                ingredients: 'huyna'
+            }
+        )
+    } finally {
+        await client.close();
+    }
+}
+
+run().catch(console.dir);
 
 app.use(express.static('public'))
 
